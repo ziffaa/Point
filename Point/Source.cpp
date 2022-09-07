d4896bc7e803dda1cc1c040d1ea176d8499f860f
ziffa <rforkaliuk@gmail.com>
2022-09-07 23:08:02 +0300

    Добавил конструкторы

--- Point/Source.cpp
+++ Point/Source.cpp
@@ -5,6 +5,17 @@ class Point
 {
     int x;
     int y;
+public:
+    Point()
+    {
+        x = 0;
+        y = 0;
+    }
+    Point(int x, int y)
+    {
+        x = x;
+        y = y;
+    }
 };
 
 int main()
